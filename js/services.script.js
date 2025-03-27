3401f1d9550cbefa19de44f36c40ddc8eca083f2
AR Assignments <49037725+ARassignments@users.noreply.github.com>
2025-03-27 05:50:39 +0500

    update

--- js/services.script.js
+++ js/services.script.js
@@ -1,41 +1,16 @@
 let services = [
-  {
-    name: "Air Duct Cleaning",
-  },
-  {
-    name: "Air Vent Cleaning",
-  },
-  {
-    name: "Dryer Vent Cleaning",
-  },
-  {
-    name: "Duct Sanizitation Process",
-  },
-  {
-    name: "Furnance Cleaning",
-  },
-  {
-    name: "Register & Return Vents",
-  },
-  {
-    name: "Blower Motor Cleaning",
-  },
-  {
-    name: "Central Vacuum",
-  },
-  {
-    name: "Deep+Brushing Cleaning",    
-  },
-  {
-    name: "Main Line Cleaning",
-  },
-  {
-    name: "Furnance Filter Cleaning & Replacement",
-  },
+  "Air Duct Cleaning",
+  "Air Vent Cleaning",
+  "Dryer Vent Cleaning",
+  "Duct Sanizitation Process",
+  "Furnance Cleaning",
+  "Register & Return Vents",
+  "Blower Motor Cleaning",
+  "Central Vacuum",
+  "Deep+Brushing Cleaning",
+  "Main Line Cleaning",
+  "Furnance Filter Cleaning & Replacement",
 ];
-
-// let categories = ["Web Design", "Web Application", "Mobile Application",
-//   "Digital Marketing"];
 
 let service_description = [
   {
@@ -44,50 +19,5 @@ let service_description = [
     pLink: "https://regularhost.com/",
     pServices: services[1],
     pDesc: `Regularhost you are looking for a personal website hosting plan or a business website hosting plan, We are the perfect solution for you. Our powerful website hosting services will not only help you achieve your overall website goals, but will also provide you with the confidence you need in knowing that you are partnered with a reliable and secure website hosting platform.`,
-  },
-];
-
-
-
-let testimonials = [
-  {
-    tName: "Zafar Ul Hasnain",
-    tPost: "",
-    tImage: "1.png",
-    tMessage: `“SMI SOFTS developed a custom software solution tailored to our business needs. Their work was timely and highly professional. Our daily operations have become much more efficient now. Highly recommended for anyone looking for reliable IT solutions”`,
-    tDate: "Feb 19, 2025",
-    tTime: "",
-    tRating: 2,
-    tPos: 1,
-  },
-  {
-    tName: "Muhammad Amjad",
-    tPost: "",
-    tImage: "2.png",
-    tMessage: `“Working with SMI SOFTS was fantastic. They built a user-friendly e-commerce website, and everything runs smoothly. We've noticed an increase in sales, and customer feedback is great”`,
-    tDate: "Jan 9, 2025",
-    tTime: "",
-    tRating: 3,
-    tPos: 0,
-  },
-  {
-    tName: "Muhammad Moiz",
-    tPost: "",
-    tImage: "3.png",
-    tMessage: `“SMI SOFTS’ digital marketing strategies have transformed our brand. Their social media campaigns and SEO efforts have greatly improved our visibility and engagement. They truly know what they’re doing”`,
-    tDate: "Dec 16, 2024",
-    tTime: "",
-    tRating: 1,
-    tPos: 0,
-  },
-  {
-    tName: "Muhammad Huzaifa",
-    tPost: "",
-    tImage: "4.png",
-    tMessage: `“Developing a mobile app with SMI SOFTS was a great experience. The team focused on user experience and functionality, which outstanding. They met all deadlines, and communication throughout the process was seamless”`,
-    tDate: "Dec 01, 2024",
-    tTime: "",
-    tRating: 4,
-    tPos: 1,
   },
 ];
